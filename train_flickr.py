0b26ee1e126901bf4af3b28d3a9f5e05ea8a8442
unknown <raphael.montaud@gmail.com>
2018-12-30 12:30:00 +0000

    debug flickr train

--- train_flickr.py
+++ train_flickr.py
@@ -8,15 +8,15 @@ if __name__ == "__main__":
 
     for i in range(5):
 
-        params = flickr_params.copy()
-        params.update(flickr_train_params)
-        my_train(params, first_arg)
-
         results_dir = "benchmark_results"
 
         if import_cuhk:
             results_dir += "_import"
             flickr_params["continue_train"] = True
+
+        params = flickr_params.copy()
+        params.update(flickr_train_params)
+        my_train(params, first_arg)
 
         my_test(flickr_params, first_arg, benchmark=True, results_dir=results_dir + str(i))
 
