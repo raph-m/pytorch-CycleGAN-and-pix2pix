7372f25840533432286bfacd8ef615aaa65ec3ce
unknown <raphael.montaud@gmail.com>
2019-01-01 19:06:17 +0100

    change ce batchsize

--- utils.py
+++ utils.py
@@ -24,7 +24,7 @@ elif env_name == "hind":
 elif env_name == "compute_engine":
     cuhk = {"batch_size": "16", "n_epochs": 100}
     flickr = {"batch_size": "8", "n_epochs": 10}
-    celeba = {"batch_size": "16", "n_epochs": 2}
+    celeba = {"batch_size": "32", "n_epochs": 2}
     local_params = {"cuhk": cuhk, "flickr": flickr, "celeba": celeba}
 
 netg = "unet_256"
