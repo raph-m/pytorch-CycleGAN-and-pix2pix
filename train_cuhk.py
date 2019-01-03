608a13a8a6106fc800f889ef55052dffe51285cb
unknown <raphael.montaud@gmail.com>
2019-01-03 20:01:24 +0100

    debug train cuhk

--- train_cuhk.py
+++ train_cuhk.py
@@ -5,14 +5,11 @@ from utils import my_train, flickr_train_params, flickr_params, my_test, copy_ge
 if __name__ == "__main__":
     first_arg = sys.argv[0]
 
-    flickr_params["epoch"] = "85"
-    my_test(flickr_params, first_arg, benchmark=True, results_dir="benchmark_results")
-
     params = cuhk_pix2pix_params.copy()
     params.update(cuhk_pix2pix_train_params)
     my_train(params, first_arg)
 
-    my_test(cuhk_cycle_params, first_arg, benchmark=True, results_dir="benchmark_results")
+    my_test(cuhk_pix2pix_params, first_arg, benchmark=True, results_dir="benchmark_results")
 
     params = cuhk_cycle_params.copy()
     params.update(cuhk_cycle_train_params)
