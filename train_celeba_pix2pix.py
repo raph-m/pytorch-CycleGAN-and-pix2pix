652c6cde02ff64a28ff920cae8177da7e6505cff
dadounhind <41945456+dadounhind@users.noreply.github.com>
2019-01-10 18:22:30 +0100

    change celebaèparam{epoch]

--- train_celeba_pix2pix.py
+++ train_celeba_pix2pix.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     params.update(celeba_pix2pix_train_params)
 
     celeba_pix2pix_params["epoch"] = "5"
-    my_test(celeba_pix2pix_params, first_arg, benchmark=True, results_dir="benchmark_results_epoch5")
+    my_test(celeba_pix2pix_params, first_arg, benchmark=True, results_dir="train_results_5")
 
     celeba_pix2pix_params["epoch"] = "10"
-    my_test(celeba_pix2pix_params, first_arg, benchmark=True, results_dir="benchmark_results")
+    my_test(celeba_pix2pix_params, first_arg, benchmark=True, results_dir="train_results_10")
 
 
 
